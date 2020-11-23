b2896fe7a672a3e076a0298f7d90ae96f0ab8dee
usuario <pablo.meca.sanchez@gmail.com>
2020-11-23 10:18:41 +0100

    ejercicio

--- src/app/notes/notes.page.ts
+++ src/app/notes/notes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Note } from '../modelo/Note';
 
 @Component({
   selector: 'app-notes',
@@ -6,15 +8,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.page.scss'],
 })
 export class NotesPage implements OnInit {
-  notes : any;
-  constructor() { 
-    this.notes = [
-      {'title':'nota1'},
-      {'title':'nota2'},
-      {'title':'nota3'}
-    ];
+  public notes : Note[]=[];
+  constructor(public alertCtrl : AlertController) {
   }
   ngOnInit() {
   }
-
+  async addNote(){
+    const alert = await this.alertCtrl.create({
+      header: 'Add note',
+      inputs:[
+        {
+          name: 'texto',
+          type:'text',
+          placeholder : 'texto'
+        }
+      ],
+      buttons:[
+        {
+          text:'cancel',
+          role:'cancel',
+          handler:()=>{
+            console.log('Confirm cancel');
+          }
+        },{
+          text:'Ok',
+          handler:(data:Note)=>{
+            console.log(data.texto);
+            this.notes.push(data);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+  async editNote(note:Note) {
+    const alert = await this.alertCtrl.create({
+      header: 'Add Note',
+      inputs: [
+        {
+          value:note.texto,
+          name: 'texto',
+          type: 'text',
+          placeholder: 'Escriba el texto'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Save',
+          handler: (data:Note) => {
+            let index = this.notes.indexOf(note);  //saca el índice del objeto dentro del array
+            if(index > -1){
+              this.notes[index] = data;
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
