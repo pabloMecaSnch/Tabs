615bb0a9402019292e4b41c18d89cff54f0b25cc
Pablo <pablo.meca.sanchez@gmail.com>
2020-11-25 17:36:30 +0100

    final

--- src/app/tabsnav/tabsnav-routing.module.ts
+++ src/app/tabsnav/tabsnav-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
   {
     path: 'contacts',
     loadChildren: ()=> import('../contacts/contacts.module').then(m => m.ContactsPageModule)
+  },
+  {
+    path: 'ladder',
+    loadChildren : () => import('../ladder/ladder.module').then(m => m.LadderPageModule)
   }
   ]
   },{
